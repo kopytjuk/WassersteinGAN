2ea9a040b786d6ef6b16226f1eb870425cd823fc
kopytjuk <kopytjuk@gmail.com>
2018-12-31 14:37:16 +0100

    Refactor generator code to create a palette on one dimension of z.

--- generate-images.ps1
+++ generate-images.ps1
@@ -1,5 +1,5 @@
 # todo: select via GUI
 $config_path = "weights/20181228_2356/generator_config.json"
 $weights_path = "weights/20181228_2356/netG_epoch_1914.pth"
-$nimages = "5"
+$nimages = "100"
 python generate.py -c "$config_path" -w "$weights_path" -o generated-images -n "$nimages" --cuda
--- generate.py
+++ generate.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import argparse
 import random
+
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.parallel
@@ -13,9 +15,75 @@ import torchvision.utils as vutils
 from torch.autograd import Variable
 import os
 import json
+from PIL import Image
 
 import models.dcgan as dcgan
 import models.mlp as mlp
+
+
+class ImageWizard():
+
+    def __init__(self, cfg_path, weights_path):
+
+        with open(cfg_path, 'r') as gencfg:
+            generator_config = json.loads(gencfg.read())
+
+        imageSize = generator_config["imageSize"]
+        nz = generator_config["nz"]
+        nc = generator_config["nc"]
+        ngf = generator_config["ngf"]
+        noBN = generator_config["noBN"]
+        ngpu = generator_config["ngpu"]
+        mlp_G = generator_config["mlp_G"]
+        n_extra_layers = generator_config["n_extra_layers"]
+
+        if noBN:
+            netG = dcgan.DCGAN_G_nobn(imageSize, nz, nc, ngf, ngpu, n_extra_layers)
+        elif mlp_G:
+            netG = mlp.MLP_G(imageSize, nz, nc, ngf, ngpu)
+        else:
+            netG = dcgan.DCGAN_G(imageSize, nz, nc, ngf, ngpu, n_extra_layers)
+
+        # load weights
+        netG.load_state_dict(torch.load(weights_path))
+
+        if torch.cuda.is_available():
+            netG = netG.cuda()
+
+        self.model = netG
+        self.nz = nz
+
+    
+    def generate_palette(self, nimages=100, dim_z=None):
+
+        z = np.random.randn(1, self.nz, 1, 1)
+        Z = np.tile(z, (nimages, 1, 1, 1))
+        Z = Z.astype(np.float32)
+
+        if dim_z is None:
+            dim_z = np.random.choice(self.nz)
+
+        # bugfix
+        Z[:, dim_z, 0, 0] = np.linspace(-1, 1, nimages)
+
+        Z = torch.tensor(Z)
+
+        if torch.cuda.is_available():
+            Z = Z.cuda()
+
+        # forward pass
+        G = self.model(Z)
+        G.data = G.data.mul(0.5).add(0.5)
+
+        images_list = list()
+        for i in range(nimages):
+            ndarr = G.data[i, ...].mul(255).clamp(0, 255).byte().permute(1, 2, 0).cpu().numpy()
+            im = Image.fromarray(ndarr)
+            images_list.append(im)
+
+        return images_list
+
+
 
 if __name__=="__main__":
 
@@ -27,37 +95,9 @@ if __name__=="__main__":
     parser.add_argument('--cuda', action='store_true', help='enables cuda')
     opt = parser.parse_args()
 
-    with open(opt.config, 'r') as gencfg:
-        generator_config = json.loads(gencfg.read())
-    
-    imageSize = generator_config["imageSize"]
-    nz = generator_config["nz"]
-    nc = generator_config["nc"]
-    ngf = generator_config["ngf"]
-    noBN = generator_config["noBN"]
-    ngpu = generator_config["ngpu"]
-    mlp_G = generator_config["mlp_G"]
-    n_extra_layers = generator_config["n_extra_layers"]
+    iw = ImageWizard(opt.config, opt.weights)
 
-    if noBN:
-        netG = dcgan.DCGAN_G_nobn(imageSize, nz, nc, ngf, ngpu, n_extra_layers)
-    elif mlp_G:
-        netG = mlp.MLP_G(imageSize, nz, nc, ngf, ngpu)
-    else:
-        netG = dcgan.DCGAN_G(imageSize, nz, nc, ngf, ngpu, n_extra_layers)
+    img_arr = iw.generate_palette(opt.nimages)
 
-    # load weights
-    netG.load_state_dict(torch.load(opt.weights))
-
-    # initialize noise
-    fixed_noise = torch.FloatTensor(opt.nimages, nz, 1, 1).normal_(0, 1)
-
-    if opt.cuda:
-        netG.cuda()
-        fixed_noise = fixed_noise.cuda()
-
-    fake = netG(fixed_noise)
-    fake.data = fake.data.mul(0.5).add(0.5)
-
-    for i in range(opt.nimages):
-        vutils.save_image(fake.data[i, ...].reshape((1, nc, imageSize, imageSize)), os.path.join(opt.output_dir, "generated_%02d.png"%i))
+    for i, img in enumerate(img_arr):
+        img.save(os.path.join(opt.output_dir, "%03d.png"%i))
